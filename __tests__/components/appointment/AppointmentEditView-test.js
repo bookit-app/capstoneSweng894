47a780bf48ae17222d3ebe6b7050c3fc228a9c3c
Standley Eugene <estand25@gmail.com>
2019-11-21 10:49:18 -0500

    Updated test from snapshot to render without crashing

--- __tests__/components/appointment/AppointmentEditView-test.js
+++ __tests__/components/appointment/AppointmentEditView-test.js
@@ -1,35 +1,50 @@
-import React from 'react'
+
 import {AppointmentEditView} from '../../../src/components/appointment'
+import React from 'react'
 import {ButtonCustom} from '../../../src/components/common/'
-import {create} from 'react-test-renderer';
-import { UpcomingAppointments } from '../../../src/constant'
+import renderer from 'react-test-renderer';
+import {shallow, mount, render} from "enzyme";
+import {UpcomingAppointments} from '../../../src/constant'
+import {act} from 'react-dom/test-utils'
+import { exportAllDeclaration } from '@babel/types';
 
 describe('appointment edit view correctly render', () => {
-    test('Matches the snapshot', () => {
-        const replaceItem = () => {}
-        const onDisplay = () => {}
-        const onEditClick = () => {
-            return (
-                <ButtonCustom>
-                    {'Edit'}
-                </ButtonCustom>
-            )
-        }
-        const appointmentEditView = create(
-            <AppointmentEditView
-                onEditClick={onEditClick()}
-                item={UpcomingAppointments[0]}
-                address={'Test'}
-                city={'Test'}
-                state={'Test'}
-                zipCode={'Test'}
-                profile={{}}
-                preference={{}}
-                token={''}
-                replaceItem={() => replaceItem()}
-                onDisplay={() => onDisplay()}
-            />
+    let props;
+
+    const replaceItem = () => {}
+    const onDisplay = () => {}
+    const onEditClick = () => {
+        return (
+            <ButtonCustom>
+                {'Edit'}
+            </ButtonCustom>
         )
-        expect(appointmentEditView.toJSON()).toMatchSnapshot();
+    }
+    const item = UpcomingAppointments[0]
+    const street_ = 'Test'
+    const city_ = 'Test'
+    const state_ = 'Test'
+    const zipCode_ = 'Test'
+    const profile={}
+    const preference={}
+    const token='Test'
+
+    it('should render the appointment edit view without crashing', () => {
+        props = {
+            onEditClick:onEditClick(),
+            item:item,
+            address:street_,
+            city:city_,
+            state:state_,
+            zipCode:zipCode_,
+            profile:profile,
+            preference:preference,
+            token:token,
+            replaceItem:replaceItem(),
+            onDisplay:onDisplay(),
+        }
+
+        const appointEditView = renderer.create(<AppointmentEditView {...props}/>).toJSON()
+        expect(appointEditView).toBeTruthy();
     })
 })
