3f1a723849a68620f12e97f96f591fb33cdf59b0
Standley Eugene <estand25@gmail.com>
2019-11-23 17:09:10 -0500

    Add Unit test for action creation, and bite of refractor to remove unneccessary commons

--- src/store/index.js
+++ src/store/index.js
@@ -327,17 +327,17 @@ export const getAppointment = (type, token) => {
                 toDate: moment(endDate).format('YYYY-MM-DD'),
             }
 
-            console.log('UpcomingAppointment', payload);
+            // console.log('UpcomingAppointment', payload);
     
             var filter = utilites.filterGenerate(payload)
             api.searchAppointmentByFilter(filter,token)
                 .then((data) => {
                     var previousAppointment = data.data
-                    console.log('getAppointment', 'previous fullfilled');
+                    // console.log('getAppointment', 'previous fullfilled');
                     dispatch(appointment.GetPreviousAppointmentFullFilled(previousAppointment))
                 })
                 .catch((err) => {
-                    console.log('getAppointment', 'previous reject');
+                    // console.log('getAppointment', 'previous reject');
                     dispatch(appointment.GetPreviousAppointmentReject(error))
                     dispatch(appointment.SetPreviousAppointment(PreviousAppointments))
                 })
@@ -355,17 +355,17 @@ export const getAppointment = (type, token) => {
                 toDate: moment(endDate).format('YYYY-MM-DD'),
             }
 
-            console.log('UpcomingAppointment', payload);
+            // console.log('UpcomingAppointment', payload);
             
             var filter = utilites.filterGenerate(payload)
             api.searchAppointmentByFilter(filter, token)
                 .then((data) => {
                     var upcomingAppointment = data.data
-                    console.log('getAppointment', 'upcoming fullfilled');
+                    // console.log('getAppointment', 'upcoming fullfilled');
                     dispatch(appointment.GetUpcomingAppointmentFullFilled(upcomingAppointment))
                 })
                 .catch((err) =>{
-                    console.log('getAppointment', 'upcoming reject');
+                    // console.log('getAppointment', 'upcoming reject');
                     dispatch(appointment.GetUpcomingAppointmentReject(error))
                     dispatch(appointment.SetUpcomingAppointment(UpcomingAppointments))
                 })
