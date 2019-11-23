6a4f311a8fba6d1e599ebd402c6ffbf024e5355b
Standley Eugene <estand25@gmail.com>
2019-11-23 12:45:02 -0500

    Update Stylist Unit Test

--- __tests__/components/appointment/Stylist-test.js
+++ __tests__/components/appointment/Stylist-test.js
@@ -6,13 +6,14 @@ import {Stylist} from '../../../src/components/appointment'
 
 describe('Stylist correctly renders', () => {
     let props
+    const onSetStylist = (i) => {}
 
     it('Stylist correctly renders with status blank', () => {
         props = {
             status: '',
             staffMemberName: 'test test',
             stylistList: [],
-            onSetStylist: () => {},
+            onSetStylist: i => onSetStylist(i),
         }
         var stylist = shallow(<Stylist {...props}/>)
         expect(stylist).toBeTruthy()
@@ -23,7 +24,7 @@ describe('Stylist correctly renders', () => {
             status: 'READY',
             staffMemberName: 'test test',
             stylistList: ['a1 a2', 'b2 b2'],
-            onSetStylist: () => {}
+            onSetStylist: i => onSetStylist(i),
         }
         var stylist = shallow(<Stylist {...props}/>)
         expect(stylist).toBeTruthy()
